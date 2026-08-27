eea3016f20a2bf35b5bda6b2464e6e481993e02e
akhil170209 <bashettyakhil1702@gmail.com>
2026-08-27 17:15:22 +0530

    yes

--- apps/5_RAG_agent.py
+++ apps/5_RAG_agent.py
@@ -573,14 +573,12 @@ else:
                 )
 
             answer = response["messages"][-1].content
-last_message = response["messages"][-1].content
 
-if isinstance(last_message, list):
-        clean_text = "".join([block.get("text", "") for block in last_message if isinstance(block, dict)])
+if isinstance(answer, list):
+        clean_text = "".join([block.get("text", "") for block in answer if isinstance(block, dict)])
 else:
-        clean_text = str(last_message)
+        clean_text = str(answer)
 
-        # Display clean response in Streamlit
         st.markdown(clean_text)
 
         # Save AI response
